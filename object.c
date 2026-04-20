f344d67668d040c253cac2a2bb9e6d93068e3e47
sagarwalikar999 <sagarwalikar999@gmail.com>
2026-04-20 16:19:45 +0000

    P1C5

--- object.c
+++ object.c
@@ -126,6 +126,45 @@ int object_read(const ObjectID *id, ObjectType *type_out, void **data_out, size_
         return -1; 
     }
 
+    uint8_t *null_byte = memchr(full_buf, '\0', file_size);
+    if (!null_byte) {
+        free(full_buf);
+        return -1; 
+    }
+
+    char type_str[16];
+    size_t claimed_size;
+    if (sscanf((char *)full_buf, "%15s %zu", type_str, &claimed_size) != 2) {
+        free(full_buf);
+        return -1;
+    }
+
+    if (strcmp(type_str, "blob") == 0) {
+        *type_out = OBJ_BLOB;
+    } else if (strcmp(type_str, "tree") == 0) {
+        *type_out = OBJ_TREE;
+    } else if (strcmp(type_str, "commit") == 0) {
+        *type_out = OBJ_COMMIT;
+    } else {
+        free(full_buf);
+        return -1;
+    }
+
+    size_t header_len = (null_byte - full_buf) + 1;
+    if (claimed_size != (size_t)(file_size - header_len)) {
+        free(full_buf);
+        return -1; 
+    }
+
+    *len_out = claimed_size;
+    *data_out = malloc(claimed_size);
+    if (!*data_out) {
+        free(full_buf);
+        return -1;
+    }
+
+    memcpy(*data_out, full_buf + header_len, claimed_size);
     free(full_buf);
-    return -1; // Stub for Commit 4
+
+    return 0;
 }
